b9e0264d7ef45e8f982df1420fcefa19089c42d1
ZakiaENZI <ccdu300@gmail.com>
2017-02-14 21:44:17 +0100

    Ajout de la page de contact

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,18 +9,6 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/homepage", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
-    }
-
-
-    /**
      * @Route("/", name="accueil")
      */
     public function accueilAction(Request $request)
@@ -28,5 +16,14 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('admin/index.html.twig');
     }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('admin/contact.html.twig');
+    }
+
 
 }
