725996def9239d8d4bfc8203700b3f6f852736e8
ZakiaENZI <ccdu300@gmail.com>
2017-02-16 19:29:46 +0100

    Ajout visualisation avec Doctrine d'un ID Ajout de l'ajout dans la DB d'une entrée !! ça ajoute mais il y a des pbm dans la vue !!

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,7 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Bank;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -26,6 +26,46 @@ class DefaultController extends Controller
     }
 
 
+    /**
+     * @Route("/test", name="test")
+     */
+    public function testAction(Request $request)
+    {
+        $bank = new Bank();
+        $bank->setMoneycents(156);
+        $bank->setMoneyint(55);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($bank);
+        $em->flush();
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+            return $this->redirect($this->generateUrl('AppBundle', array('id' => $bank->getId())));
+        }
+        // replace this example code with whatever you need
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+
+    /**
+     * @Route("/test2/{id}", name="test2")
+     */
+    public function test2Action($id)
+    {
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Bank')
+        ;
+        $advert = $repository->find($id);
+        if (null === $advert) {
+            throw new NotFoundHttpException("L'id de bank ".$id." n'existe pas.");
+        }
+        $id = $advert->getIdbank();
+        // replace this example code with whatever you need
+        return $this->render('default/testView.html.twig', array(
+            'advertid' => $advert->getIdbank(),
+            'advertcent' => $advert->getMoneycents(),
+            'advertint' => $advert->getMoneyint()
+        ));
+    }
 
     /**
      * @Route("/nojs", name="nojs")
