eaaa79bc748331021a68c3a5608999140022edf5
DESKTOP-K1S8UF2 <ccdu300@gmail.com>
2017-02-18 19:54:01 +0100

    Fos User Bundle !! No template !!

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,10 +5,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Bank;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Validator\Constraints\DateTime;
 
 class DefaultController extends Controller
 {
