79d1257b5d1b2801807a69f508802e979715261c
Joris Brandt <jorisbrandt@gmail.com>
2017-03-19 22:31:44 +0100

    fonction JS pour ordonner les tables en cliquant sur le head

--- web/js/script.js
+++ web/js/script.js
@@ -2,51 +2,53 @@
  * Created by cedric on 16/02/2017.
  */
 
-    $('#submit').on('click',function (e) {
-        e.preventDefault();
-        var id = $('#id').val(),
-            offset = $('#offset').val(),
-            table = $('#table').val(),
-            url = "/ajaxViewMultipleBank/"+id+"/"+offset+'/'+table;
-        $.ajax({
-            url: url,
-            method: 'GET',
-            dataType: 'html'
-        }).done(function (result) {
-            $('#result').fadeOut();
-            $('#result').empty().hide().append(result).fadeIn();
-            $('button.viewAction').on('click',function(){
-                var id = this.value,
-                    table = this.name,
-                    url = "/ajaxViewRow/"+id+'/'+table;
-                $.ajax({
-                    url: url,
-                    method: 'GET',
-                    dataType: 'html'
-                }).done(function (result) {
-                    $('#result').fadeOut();
-                    $('#result').empty().hide().append(result).fadeIn();
-                });
+$('#submit').on('click',function (e) {
+    e.preventDefault();
+    var id = $('#id').val(),
+        offset = $('#offset').val(),
+        table = $('#table').val(),
+        url = "/ajaxViewMultipleBank/"+id+"/"+offset+'/'+table;
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'html'
+    }).done(function (result) {
+        $('#result').fadeOut();
+        $('#result').empty().hide().append(result).fadeIn();
+        $('button.viewAction').on('click',function(){
+            var id = this.value,
+                table = this.name,
+                url = "/ajaxViewRow/"+id+'/'+table;
+            $.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'html'
+            }).done(function (result) {
+                $('#result').fadeOut();
+                $('#result').empty().hide().append(result).fadeIn();
             });
         });
     });
+});
+
+$("#result").on('click', 'th.tablehead', function (e) {
+    var theTarget = $(e.target);
+    var table = $("#"+ theTarget.data("table"));
+    sortTable(table);
+})
+
+function sortTable(table) {
+    var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+    this.asc = !this.asc
+    if (!this.asc){rows = rows.reverse()}
+    for (var i = 0; i < rows.length; i++){table.append(rows[i])}
 }
-
-/**
- *
- * @param table - table à effectuer sort
- * @param order - ordre à respecter
- */
-
-function sortTable(table, order) {
-    var asc   = order === 'asc',
-        tbody = table.find('tbody');
-
-    tbody.find('tr').sort(function(a, b) {
-        if (asc) {
-            return $('td:first', a).text().localeCompare($('td:first', b).text());
-        } else {
-            return $('td:first', b).text().localeCompare($('td:first', a).text());
-        }
-    }).appendTo(tbody);
+function comparer(index) {
+    return function(a, b) {
+        var valA = getCellValue(a, index), valB = getCellValue(b, index)
+        return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.localeCompare(valB)
+    }
+}
+function getCellValue(row, index){
+    return $(row).children('td').eq(index).html()
 }
