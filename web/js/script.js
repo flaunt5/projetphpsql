25e1fa81ed0640c24c0a62d9e7636ae5382d5848
flauntfive <jorisbrandt@gmail.com>
2017-03-20 13:19:22 +0100

    correctif

--- web/js/script.js
+++ web/js/script.js
@@ -2,32 +2,31 @@
  * Created by cedric on 16/02/2017.
  */
 
-    $('#submit').on('click',function (e) {
-        e.preventDefault();
-        var id = $('#id').val(),
-            offset = $('#offset').val(),
-            table = $('#table').val(),
-            url = "/ajaxViewMultipleBank/"+id+"/"+offset+'/'+table;
-        $.ajax({
-            url: url,
-            method: 'GET',
-            dataType: 'html'
-        }).done(function (result) {
-            $('#result').fadeOut();
-            $('#result').empty().hide().append(result).fadeIn();
-            $('button.viewAction').on('click',function(){
-                var id = this.value,
-                    table = this.name,
-                    url = "/ajaxViewRow/"+id+'/'+table;
-                /*$.ajax({
-                    url: url,
-                    method: 'GET',
-                    dataType: 'html'
-                }).done(function (result) {
-                    $('#result').fadeOut();
-                    $('#result').empty().hide().append(result).fadeIn();
-                });*/
-            });
+$('#submit').on('click',function (e) {
+    e.preventDefault();
+    var id = $('#id').val(),
+        offset = $('#offset').val(),
+        table = $('#table').val(),
+        url = "/ajaxViewMultipleBank/"+id+"/"+offset+'/'+table;
+    $.ajax({
+        url: url,
+        method: 'GET',
+        dataType: 'html'
+    }).done(function (result) {
+        $('#result').fadeOut();
+        $('#result').empty().hide().append(result).fadeIn();
+        $('button.viewAction').on('click',function(){
+            var id = this.value,
+                table = this.name,
+                url = "/ajaxViewRow/"+id+'/'+table;
+            /*$.ajax({
+                url: url,
+                method: 'GET',
+                dataType: 'html'
+            }).done(function (result) {
+                $('#result').fadeOut();
+                $('#result').empty().hide().append(result).fadeIn();
+            });*/
         });
     });
 });
